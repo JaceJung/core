ecfed31325b6e496690d7a71bc5e49fb981e1a83
JaceJung <ksjung330@gmail.com>
2021-05-19 15:11:28 +0900

    spring code

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -6,21 +6,29 @@ import hello.core.member.*;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
         return new RateDiscountPolicy();
     }
 
+    @Bean
     public MemberRepository memberRepository(){
         return new MemmoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
